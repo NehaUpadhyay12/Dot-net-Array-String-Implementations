aeffb58b1a168b4ea30a505a12e44e734110dbf3
Neha Upadhyay <nehaupadhyay@NehaMac.local>
2019-02-13 00:51:26 +0530

    Reversing null terminated string

---
+++ Dot-net-Array-String-Implementations/ReverseNullTerminatedString/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+
+namespace ReverseNullTerminatedString
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            StringBuilder s = new StringBuilder("neha1\0");
+            Console.WriteLine($"Actual String is {s.ToString()}");
+            Reverse(s);
+            
+        }
+
+        static void Reverse(StringBuilder str)
+        {
+            int end = str.Length - 1;
+            if(end == 0)
+            {
+                Console.WriteLine("String is empty");
+                return;
+            }
+
+            for (int i = 0; i <= (end - 1)/2; i++)
+            {
+                char temp = str[i];
+                str[i] = str[end - i - 1];
+                str[end - i - 1] = temp;
+            }
+
+            Console.WriteLine($"Reverse string is {str.ToString()}");
+        }
+    }
+}
